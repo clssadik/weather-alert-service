ed4b97ac5d23c4a8f8d796c10eb115920821b4bb
clssadik <cillsadik@gmail.com>
2026-05-21 14:18:00 +0300

    Add location reverse geocoding, rename response/data variables

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from twilio.rest import Client
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+LOC_KEY = os.getenv("LOCATION_IQ_TOKEN")
 LAT = 36.771297
 LONG = 34.569662
 URL = "https://api.openweathermap.org/data/2.5/forecast"
+LOCATION = f"https://us1.locationiq.com/v1/reverse?key={}&lat={LAT}&lon={LONG}&format=json&"
 
 params = {
     "lat": LAT,
@@ -18,35 +20,41 @@ params = {
     "units": "metric"
 }
 
-response = requests.get(URL, params=params)
-data = response.json()
+response_weather = requests.get(URL, params=params)
+data_weather = response_weather.json()
+print(response_weather.status_code)
 
-temp = 0
-for i in range(0,8):
-    temp += data["list"][i]["main"]["temp"]
+response_location = requests.get(LOCATION)
+data_location = response_location.json()
+print(response_location.status_code)
 
-temp_24h = temp / 8
-temp_24h = round(temp_24h,1)
 
-print(response.status_code)
+# temp = 0
+# for i in range(0,8):
+#     temp += data["list"][i]["main"]["temp"]
 
-will_rain = False
+# temp_24h = temp / 8
+# temp_24h = round(temp_24h,1)
 
-for i in range(0, 5):
-    if data["list"][i]["weather"][0]["id"] > 700:
-        will_rain = True
-    # else:
-    #     print("Don't you dare :) ")
+# print(response.status_code)
 
-if will_rain:
-    account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-    client = Client(account_sid, auth_token)
+# will_rain = False
 
-    message = client.messages.create(
-        from_='whatsapp:+14155238886',
-        body= f"yağmur yağıyor, önümüzdeki 24 saatlik ortalama sıcaklık : {temp_24h}",
-        to='whatsapp:+905073519085'
-    )
+# for i in range(0, 5):
+#     if data["list"][i]["weather"][0]["id"] > 700:
+#         will_rain = True
+#     # else:
+#     #     print("Don't you dare :) ")
 
-    print(message.status)
+# if will_rain:
+#     account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+#     auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+#     client = Client(account_sid, auth_token)
+
+#     message = client.messages.create(
+#         from_='whatsapp:+14155238886',
+#         body= f"yağmur yağıyor, önümüzdeki 24 saatlik ortalama sıcaklık : {temp_24h}",
+#         to='whatsapp:+905073519085'
+#     )
+
+#     print(message.status)
