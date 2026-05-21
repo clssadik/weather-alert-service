08cb96259ef558fc6b77dd3c70aec9f207a36ce0
clssadik <cillsadik@gmail.com>
2026-05-21 17:21:14 +0300

    Restore temp calculation and Twilio message, add logic.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 import requests
 from dotenv import load_dotenv
 from twilio.rest import Client
-
-
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 LOC_KEY = os.getenv("LOCATION_IQ_TOKEN")
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
 LAT = 36.771297
 LONG = 34.569662
 URL = "https://api.openweathermap.org/data/2.5/forecast"
@@ -17,44 +19,29 @@ params = {
     "lat": LAT,
     "lon": LONG,
     "appid": API_KEY,
-    "units": "metric"
+    "units": "metric",
+    "lang": "tr",
 }
 
 response_weather = requests.get(URL, params=params)
 data_weather = response_weather.json()
-print(response_weather.status_code)
 
 response_location = requests.get(LOCATION)
 data_location = response_location.json()
-print(response_location.status_code)
 
 
-# temp = 0
-# for i in range(0,8):
-#     temp += data["list"][i]["main"]["temp"]
+temp = 0
+for i in range(0,8):
+    temp += data["list"][i]["main"]["temp"]
 
-# temp_24h = temp / 8
-# temp_24h = round(temp_24h,1)
+temp_24h = temp / 8
+temp_24h = round(temp_24h,1)
 
-# print(response.status_code)
 
-# will_rain = False
 
-# for i in range(0, 5):
-#     if data["list"][i]["weather"][0]["id"] > 700:
-#         will_rain = True
-#     # else:
-#     #     print("Don't you dare :) ")
 
-# if will_rain:
-#     account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-#     auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-#     client = Client(account_sid, auth_token)
-
-#     message = client.messages.create(
-#         from_='whatsapp:+14155238886',
-#         body= f"yağmur yağıyor, önümüzdeki 24 saatlik ortalama sıcaklık : {temp_24h}",
-#         to='whatsapp:+905073519085'
-#     )
-
-#     print(message.status)
+message = client.messages.create(
+    from_='whatsapp:+14155238886',
+    body= f"yağmur yağıyor, önümüzdeki 24 saatlik ortalama sıcaklık : {temp_24h}",
+    to='whatsapp:+905073519085'
+)
